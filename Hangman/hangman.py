8d19be750b1dfe698f1aad4b2201100b07d6ddb9
Dhruv <dhruv.g123@gmail.com>
2023-01-29 15:42:16 -0800

    added concept of lives

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -20,31 +20,41 @@ def get_valid_words(words):
 
 def hangman():
     words = load_data()
-    # print(f'length of list: {len(words)}')
     word = get_valid_words(words).upper()
     word_letters = set(word)    # letters in the word
     alphabet = set(string.ascii_uppercase)
     used_letters = set()    # what user has guessed
 
+    lives = 6
+
     # get user input 
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         # letters used
-        print('You have used these letters: ',''.join(used_letters))
+        print(f'You have {lives} remaining and you have used these letters: ',''.join(used_letters))
 
         # what currently word is (e.g. W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
-        print('Current word: ',''.join(word_list))
+        print('Current word: ',''.join(word_list), '\n')
 
         user_input_letter = input('Guess a letter: ').upper()
         if user_input_letter in alphabet - used_letters:
             used_letters.add(user_input_letter)
             if user_input_letter in word_letters:
                 word_letters.remove(user_input_letter)
+            
+            else:
+                lives = lives - 1   # takes awat a life for wrong guess
+                print(f'Letter {user_input_letter} is not in the word.')
 
         elif user_input_letter in used_letters:
             print("You've already used that letter. Please try again.")
 
         else:
             print("Invalid letter. Please try again.")
+
+    if word_letters == 0:
+        print(f'Awesome you guessed the word: {word}')
+    else:
+        print(f'Sorry you lost, the word was: {word}')
 
 hangman()
