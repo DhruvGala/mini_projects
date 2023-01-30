2be6ace763fede9803ca327a8a26047aa7f14946
Dhruv <dhruv.g123@gmail.com>
2023-01-29 16:39:13 -0800

    added player logic

--- Tic_Tac_Toe/player.py
+++ Tic_Tac_Toe/player.py
@@ -15,11 +15,27 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        # get a random valid spot for our next move
+        square = random.choice(game.available_moves())
+        return square
 
 class HumanPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9):')
+            # we're going to check if user inputs a correct value i.e. int
+            # also we want to check if the enterd spot is available
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again.')
+
+        return val
