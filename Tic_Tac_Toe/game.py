a4ca3314f2e0d8e3e3c77d1d5c7302f3665c67a2
Dhruv <dhruv.g123@gmail.com>
2023-01-29 16:39:33 -0800

    added game logic

--- Tic_Tac_Toe/game.py
+++ Tic_Tac_Toe/game.py
@@ -23,3 +23,42 @@ class TickTacToe:
         #     if spot == ' ':
         #         moves.append(i)
         # return moves
+    
+    def empty_squares(self):
+        return ' ' in self.board
+
+    def num_empty_square(self):
+        return self.board.count(' ')
+
+    def make_move(self, square, letter):
+        # if a valid move, then assign the letter to the said square
+        # return true, else just return false.
+        if self.board[square] == ' ':
+            self.board[square] = letter
+            return True
+        else:
+            return False
+
+
+def play(game, x_player, o_player, print_game=True):
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X'    # starting letter
+    # iterate while the game still has empty squares.
+    while game.empty_square():
+        # get the move from appropriate player
+        if letter == 'O':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+
+        if game.make_move(square, letter):
+            if print_game:
+                print(letter + f' makes a move to square {square}')
+                game.print_board()
+                print('')
+
+            letter = 'O' if letter == 'X' else 'X'
+
+        
