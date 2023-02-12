237264f5ae4898985e9854f83f12f681b23a5896
Dhruv <dhruv.g123@gmail.com>
2023-02-12 11:24:33 -0800

    getting random from predefined array

--- javascript-projects/01-color-flipper/app.js
+++ javascript-projects/01-color-flipper/app.js
@@ -3,9 +3,12 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function(){
-    //TODO get random number
-    const randomNumber = 2;
+    const randomNumber = getRandomNumer();
 
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
+
+function getRandomNumer() {
+    return Math.floor(Math.random() * colors.length);
+}
